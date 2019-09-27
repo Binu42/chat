99869dc690dfcedd63810d0c18627fb9965a8edf
Binu <kbinu42@gmail.com>
2019-09-27 18:37:30 +0530

    intial commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const port = process.env.PORT || 3000;
+const publicPathDirectory = path.join('__dirname', '../public');
+
+app.use(express.static(publicPathDirectory));
+
+let count = 0;
+
+io.on('connection', (socket)=> {
+    console.log('New websocket connection');
+    socket.emit('countUpdated', count);
+
+    socket.on('increment', ()=> {
+        count++
+        io.emit('countUpdated', count);
+    })
+})
+
+server.listen(port, ()=> {
+    console.log(`server is running at ${port}`);
+})
